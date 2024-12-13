b96d8d63386b46d9936917a866962904d2c9cbaf
KaiserAce <lester.ignacio1209@gmail.com>
2024-12-13 22:40:11 +0800

    Queried database

--- init.sql
+++ init.sql
@@ -4,16 +4,17 @@ CREATE TABLE IF NOT EXISTS products (
   Product_Name VARCHAR(255),
   Product_Category ENUM('Cat', 'Food', 'Toy', 'Others'),
   Image_File VARCHAR(255),
+  Product_Price DECIMAL(8, 2) DEFAULT 10.00,
   Quantity INT DEFAULT 10,
   PRIMARY KEY (Product_ID)
 );
 
 INSERT INTO products (Product_ID, Product_Name, Product_Category, Image_File)
 VALUES
-(001, 'Cookies and Cream', 'Cat', '001.png'),
-(002, 'Garfield', 'Cat', '002.png'),
-(101, 'Dry Food', 'Food', '101.png'),
-(102, 'Canned Tuna', 'Food', '102.png'),
-(201, 'Mouse Toy', 'Toy', '201.png'),
-(202, 'Laser Pointer', 'Toy', '202.png'),
-(301, 'Cat Tree', 'Others', '301.png');
+(001, 'Cookies and Cream', 'Cat', '001.jpg'),
+(002, 'Garfield', 'Cat', '002.jpg'),
+(101, 'Dry Food', 'Food', '101.jpg'),
+(102, 'Canned Tuna', 'Food', '102.jpg'),
+(201, 'Mouse Toy', 'Toy', '201.jpg'),
+(202, 'Laser Pointer', 'Toy', '202.jpg'),
+(301, 'Cat Tree', 'Others', '301.jpg');
--- script.js
+++ script.js
@@ -1,32 +1,27 @@
-function loadItems(category) {
-  fetch('db_content.php')
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return response.json(); // Parse JSON response
-    })
-    .then(data => {
-      const contentContainer = document.getElementById('content');
-      contentContainer.innerHTML = '';
-      console.log(data);
-      data.forEach(item => {
-        if (item.Product_Category == category || category == '*'){
-          const cardElement = document.createElement('div');
-          cardElement.classList.add('card');
-          const itemElement = document.createElement('div');
-          itemElement.classList.add("content-item");
-          itemElement.innerHTML = `
-            <p>${item.Product_Name}</p>
-            <p>${item.Quantity}</p>
-          `;
-          cardElement.appendChild(itemElement);
-          contentContainer.appendChild(cardElement);
-        }
-      });
-    })
-    .catch(error => {
-        console.error('Error fetching data:', error);
-    });
+async function loadItems(category) {
+  const response =  await fetch("db_content.php");
+  const data = await response.json();
+  const contentContainer = document.getElementById('content');
+  contentContainer.innerHTML = '';
+  console.log(data);
+  data.forEach(item => {
+    if (item.Product_Category == category || category == '*'){
+      const cardElement = document.createElement('div');
+      cardElement.classList.add('card');
+      const itemElement = document.createElement('div');
+      itemElement.classList.add("content-item");
+      const imgLoc = "./images/".concat(item.Image_File);
+      itemElement.innerHTML = `
+        <div class='image-container'>
+          <img src=${imgLoc}>
+        </div>
+        <p style='font-size:30px; line-height: 0.8'>${item.Product_Name}</p>
+        <p style='font-size:20px'>$${item.Product_Price}</p>
+        <p>Qty: ${item.Quantity}</p>
+      `;
+      cardElement.appendChild(itemElement);
+      contentContainer.appendChild(cardElement);
+    }
+  });
 }
-window.onload(loadItems('*'));
+window.onload = loadItems('*');
